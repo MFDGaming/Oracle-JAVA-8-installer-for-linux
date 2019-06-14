fbcd5a36093b849b1d568ed7697c4e4e9f90d7f2
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2019-06-14 21:34:40 +0300

    Updated

--- oracle-java8-installer.sh
+++ oracle-java8-installer.sh
@@ -5,7 +5,7 @@
 case "$1" in
 	arm)
 #!/bin/bash
-apt update && apt install wget -y && wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -O "java.tar.gz" "https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-8u201-linux-arm32-vfp-hflt.tar.gz" && tar -C /opt -zxvf "java.tar.gz" && rm "java.tar.gz" && update-alternatives --install /usr/bin/java java /opt/jdk1.8.0_201/jre/bin/java 2000 && echo /opt/jdk1.8.0_201/jre/lib/arm/jli | tee -a /etc/ld.so.conf && ldconfig
+apt update && apt install wget -y && wget "https://raw.githubusercontent.com/MFDGaming/Oracle-JAVA-8-installer-for-linux/master/java_arm.tar.gz -O java.tar.gz" && mkdir "/opt/java" && tar -C /opt/java -zxvf "java.tar.gz" && rm "java.tar.gz" && update-alternatives --install /usr/bin/java java /opt/java/jre/bin/java 2000 && echo /opt/java/jre/lib/arm/jli | tee -a /etc/ld.so.conf && ldconfig
 	    ;;
 	arm64)
 
